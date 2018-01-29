bcc7d0c88ebbe827195f75a4fe8a1ecae21ad68f
isara_fahad <saraalotai>
2018-01-30 02:19:30 +0300

    New changes

--- YourLawyer/ViewController.swift
+++ YourLawyer/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello wold ")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
