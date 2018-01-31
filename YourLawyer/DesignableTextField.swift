6fb8e8ad11a39a9957af3a8f8ab7c498a26d1bcc
isara_fahad <saraalotai>
2018-01-31 16:14:29 +0300

    Interface

--- YourLawyer/DesignableTextField.swift
+++ YourLawyer/DesignableTextField.swift
@@ -18,9 +18,11 @@ class DesignableTextField: UITextField {
     func updateView(){
         if let image = rightImage {
             rightViewMode = .always
-            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+            let imageView = UIImageView(frame: CGRect(x: 5, y: 0, width: 20, height: 20))
            imageView.image = image
-            rightView = imageView
+            let view = UIView(frame: CGRect(x: 5, y: 0, width: 25, height: 20))
+            view.addSubview(imageView)
+            rightView = view
         }
         else {
             //image is nil
