292968166d6f2252c5d18493bcaf635da85fca68
isara_fahad <saraalotai>
2018-02-01 19:52:04 +0300

    complete register

---
+++ YourLawyer/LawyerRegister.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  YourLawyer
+//
+//  Created by mac pro retina on 2/1/18.
+//  Copyright © 2018 mac pro retina. All rights reserved.
+//
+import UIKit
+
+class LawyerRegister: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
