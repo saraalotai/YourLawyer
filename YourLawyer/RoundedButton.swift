e8a19c91bf1add2094a07fabe82baa6861520a4b
Munira <muniraalotai@gmail.com>
2018-02-01 23:49:09 +0300

    new screans

---
+++ YourLawyer/RoundedButton.swift
@@ -0,0 +1,24 @@
+//
+//  RoundedButton.swift
+//  YourLawyer
+//
+//  Created by mac on ١٥ جما١، ١٤٣٩ هـ.
+//  Copyright © ١٤٣٩ هـ mac pro retina. All rights reserved.
+//
+
+import UIKit
+class RoundedButton: UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderWidth = 2/UIScreen.main.nativeScale
+        contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        titleLabel?.adjustsFontForContentSizeCategory = true
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.height/2
+        layer.borderColor = isEnabled ? tintColor.cgColor : UIColor.lightGray.cgColor
+    }
+}
