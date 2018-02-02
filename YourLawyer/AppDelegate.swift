545147a2e80f290ee6dae89855635562ed5e1245
Munira <muniraalotai@gmail.com>
2018-02-02 15:49:17 +0300

    nav bar

--- YourLawyer/AppDelegate.swift
+++ YourLawyer/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //change the backgriund of nav bar
+        let  navimage = UIImage(named: "back.png")
+        UINavigationBar.appearance().setBackgroundImage(navimage, for: .default)
+    
+        
         return true
     }
 
