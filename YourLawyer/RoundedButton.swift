f4694e6f762ef8579abdfd5b68f4f522df6a9352
isara_fahad <saraalotai>
2018-02-06 22:33:52 +0300

    services

--- YourLawyer/RoundedButton.swift
+++ YourLawyer/RoundedButton.swift
@@ -10,20 +10,21 @@ import UIKit
 class RoundedButton: UIButton {
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        layer.borderWidth = 2/UIScreen.main.nativeScale
+       // layer.borderColor = UIColor.white.cgColor
+        //layer.borderWidth = 4/UIScreen.main.nativeScale
         contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         titleLabel?.adjustsFontForContentSizeCategory = true
     }
     
 override func layoutSubviews() {
     super.layoutSubviews()
+    
     layer.shadowColor = UIColor.black.cgColor
     layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
     layer.masksToBounds = false
     layer.shadowRadius = 1.0
     layer.shadowOpacity = 0.5
     layer.cornerRadius = frame.height/2
-    layer.borderColor = isEnabled ? tintColor.cgColor : UIColor.lightGray.cgColor
+    layer.borderColor = isEnabled ? tintColor.cgColor : UIColor.white.cgColor
 }
 }
