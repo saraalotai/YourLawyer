d2386421ab13188a0e226a688a57f0cfde9bf564
SaraFahad <Saaarrafahad11@gmail.com>
2018-02-09 02:16:38 -0800

    notification

---
+++ CTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CTableViewCell.swift
+//  YourLawyer
+//
+//  Created by MAC on 23/05/1439 AH.
+//  Copyright © 1439 mac pro retina. All rights reserved.
+//
+
+import UIKit
+
+class CTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var cellView: UIView!
+    
+    @IBOutlet weak var notifi: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
